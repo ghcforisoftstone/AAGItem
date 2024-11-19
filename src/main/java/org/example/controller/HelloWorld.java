98aee5883972dc11ce8b423d761f76f46f281b14
hcgaoc <hcgaoc@isoftstone.com>
2024-11-19 16:24:32 +0800

    更新pom配置

--- src/main/java/org/example/controller/HelloWorld.java
+++ src/main/java/org/example/controller/HelloWorld.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
 public class HelloWorld {
     @RequestMapping("/hello")
     public String hello(){
