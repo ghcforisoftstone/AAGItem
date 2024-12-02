32070350d253786df61788f405999aaad96404e2
hcgaoc <hcgaoc@isoftstone.com>
2024-12-02 18:19:05 +0800

    fix(scripts): 更新 start_server.sh 脚本功能
    - 添加检查并杀死已存在的 Java 进程的功能
    - 修正启动 Java 应用程序时使用变量 $JAR_FILE_PATH 替代硬编码的 JAR 文件路径

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,6 +6,17 @@ JAR_FILE_PATH="/home/ec2-user/AAGItem-1.0-SNAPSHOT.jar"
 # 定义JVM选项
 JAVA_OPTS="-Xms512m -Xmx1024m"
 
+# 使用pgrep查找jar包对应的Java进程
+PIDS=$(pgrep -f $JAR_FILE_PATH)
+
+if [ -z "$PIDS" ]; then
+    echo "No running $JAR_FILE_PATH process found."
+else
+    echo "Killing $JAR_FILE_PATH processes: $PIDS"
+    kill -9 $PIDS 2>/dev/null
+fi
+
+
 # 检查JAR文件是否存在
 if [ ! -f "$JAR_FILE_PATH" ]; then
     echo "JAR文件不存在: $JAR_FILE_PATH"
@@ -13,4 +24,4 @@ if [ ! -f "$JAR_FILE_PATH" ]; then
 fi
 
 # 启动Java应用程序
-exec nohup java -jar /home/ec2-user/AAGItem-1.0-SNAPSHOT.jar  >  /home/ec2-user/my-project.log 2>&1 &
+exec nohup java -jar $JAR_FILE_PATH  >  /home/ec2-user/my-project.log 2>&1 &
